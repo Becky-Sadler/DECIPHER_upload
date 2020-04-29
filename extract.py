d78e53eeab7aafd87c27bab9b6aad3a661d776d5
Becca Sadler <beccasadler@MacBook-Air.local>
2020-04-29 13:05:51 +0100

    Changes due to trying to filter out only patient lines in the csv file - still ongoing

--- extract.py
+++ extract.py
@@ -31,6 +31,8 @@ csvwriter.writerow(extract_head)
 
 count = 0
 
+# Creation of a for loop to go through each mutation (variant) in the .mut file
+
 for member in root.findall('Mutation'):
     assembly = member.attrib['refAssembly']
     #print(assembly)
@@ -42,7 +44,7 @@ for member in root.findall('Mutation'):
     vartype = member.find('Variant').attrib['type']
     # print(vartype)
 
-
+    # Extracting the information that is specific to the different variant types.
     if vartype == 'Substitution':
 	    position = member.find('Variant').attrib['pos']
 	    #print(position)
@@ -81,6 +83,7 @@ for member in root.findall('Mutation'):
 
     #print(classification)
 
+    # Loop to extract all the occurences for each variant
     for child in member.findall('Occurrences/Occurrence'):
         if child.find('Patient').text != None:
         	patientID = child.find('Patient').text
@@ -108,6 +111,7 @@ for member in root.findall('Mutation'):
         else: 
         	comment = None 
 
+        # Adding a row to the csv file for each occurence (variation based on the variant type)
         if vartype == 'Substitution':
         	csvwriter.writerow([assembly, chromosome, gene, vartype, position, refallele, altallele, None, None, None, transcript, classification, patientID, familyID, phenotype, comment])
         elif vartype == 'Deletion' or vartype == 'Duplication':
@@ -128,25 +132,43 @@ if lines == (count +1):
 else:
 	print('The count is ' + str(count + 1) + ' the number of lines in the csv file is ' + str(lines))
 
-
-with open(csvname) as csvfile:
-    reader = csv.DictReader(csvfile, delimiter = ",")
-    dicts = list(reader) 
-
 # Matches patientIDs using separate regexes and or statements(|) (initials followed by ( or space | O number | Location | (initials) | initials on their own. 
-patient_identifiers = re.compile(r'^([A-Z]{1,3}[ \(])|(O\d+)|(\d*[A-Z](\d){1,2})|^[\( ]([A-Z])+[\) ]$|^([A-Z]){1,3}$')
-remove_journal = re.compile(r'^((?!et al).)*$')
+patient_identifiers = re.compile(r'^([A-Z]{1,4}[ \(])|(O\d+)|(\d*[A-Z](\d){1,2})|^[\( ]([A-Z])+[\) ]$|^([A-Z]){1,4}$')
+# Matches patientIDs that contain the words in the list searchlist. 
+searchlist = ['et al', '[Rr]eview', 'LOVD', 'HCMR', 'ESP', 'NCBI', '[iI]nvestigation', 'HCM', 'Reclassification', 'ClinVar']
+words = re.compile("|".join(searchlist))
+# Matches family_IDs (CAR/GEN)
 family_id = re.compile(r'(CAR[\d]{1,5})|(GEN[\d]{1,5})| (\d){1,5}')
 
-for line in dicts:
-	print(line['FamilyID'])
+with open(csvname) as csvfile:
+    reader = csv.DictReader(csvfile, delimiter = ",") 
+    dicts = list(reader)
+    test_patient = []
+    test_family = []
 
-for line in dicts:
-	z = re.match(patient_identifiers, line['PatientID'])
-	x = re.match(remove_journal, line['PatientID'])
-	y = re.match(family_id, line['FamilyID'])
-	if z and x:
-		print(line['PatientID'] + ' ' + line['FamilyID'])
-	elif y:
-		print(line['PatientID'] + ' ' + line['FamilyID'])
+    for line in dicts:
+    	patient = re.match(patient_identifiers, line['PatientID'])
+    	family = re.match(family_id, line['FamilyID'])
+    	remove = re.match(words, line['PatientID'])
+    	if patient or family:
+		    if remove:
+		    	print(line['PatientID'] + ' ' + line['FamilyID'])
+		    else:
+		    	test_patient.append(line['PatientID'])
+		    	test_family.append(line['FamilyID'])
+
+with open('MYH7_chop.csv') as csvfile:
+	reader = csv.DictReader(csvfile, delimiter = ',')
+	dicts = list(reader)
+	known_patient = []
+	known_family = []
+	for line in dicts:
+		known_patient.append(line['PatientID'])
+		known_family.append(line['FamilyID'])
+
+known = list(zip(known_patient, known_family))
+test = list(zip(test_patient, test_family))
+
+missing = [x for x in known if x not in test]
+print(missing)
 
