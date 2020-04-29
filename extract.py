e159dab5c9a84f65b1e4774666f187fa13224089
Becca Sadler <beccasadler@MacBook-Air.local>
2020-04-29 12:30:43 +0100

    Addition of code to create csv file to write to.

--- extract.py
+++ extract.py
@@ -1,3 +1,23 @@
 import xml.etree.ElementTree as ET
+import csv
 
 tree = ET.parse('MYH7.mut')
+root = tree.getroot()
+
+# open a file (in a temporary directory) to write the data into.
+
+extract_data = open('/tmp/extract_data.csv', 'w')
+
+# create csv writer object 
+
+csvwriter = csv.writer(extract_data)
+extract_head = []
+
+
+
+
+
+
+
+
+extract_data.close() 
