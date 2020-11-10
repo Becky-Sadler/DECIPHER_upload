48c07ae574b6538777a871535602699cacc74087
Becky-Sadler <57992319+Becky-Sadler@users.noreply.github.com>
2020-11-10 16:36:29 +0000

    Restructuring upload.py

--- upload.py
+++ upload.py
@@ -2,23 +2,11 @@ import requests
 import json
 import yaml
 from requests import HTTPError
-import glob
+import pandas as pd 
 import re
-import csv
+import glob
 
-# creating a header variable with the authentication and content_type information - does not need to be repeated as it is uniform
-
-config = yaml.safe_load(open("DECIPHER_config.yml"))
- 
-system_key = (config['X-Auth-Token-System'])
-user_key = (config['X-Auth-Token-User'])
-
-
-keys = {
-    'X-Auth-Token-System':system_key,
-    'X-Auth-Token-User':user_key,
-    'Content-Type': 'application/json',
-}
+from pathlib import Path
 
 # GET request
 
@@ -42,12 +30,140 @@ def POST(url, header, data):
     else:
         print(response.content)
 
-
 # URL function 
 
 def URL(path):
     url = 'https://decipher.sanger.ac.uk/API' + path
     return url 
+
+def create_patient(project_id ,reference):
+    # Creation of a patient dictionary containing all the required fields 
+    patient = {}
+    patient['project_id'] = project_id
+    patient['sex'] = 'unknown'
+    patient['reference'] = reference 
+    
+    # Creating the patient on DECIPHER - first URL is assigned.
+    url = URL('/projects/{0}/patients'.format(project_id))
+    
+    # Dictionary is converted to JSON - need to put [] around the dictionary so the resulting json is in the right format.
+    patientdata = json.dumps([patient])
+    
+    # Patient is created within DECIPHER & the patients_id is found 
+    response = POST(url, keys, patientdata)
+    patient = response.json()
+    patients_id = (patient[0]['patient_id'])
+    return patients_id
+
+# Function to post SNV after collecting required fields 
+def post_SNV(patients_id, i):
+    snv_url = URL('/patients/{0}/snvs'.format(patients_id))
+    snv = {}
+    snv['patient_id'] = patients_id
+
+    if i['Assembly'] == 'GRCh37':
+        snv['assembly'] = 'GRCh37/hg19'
+    elif i['Assembly'] == 'GRCh38':
+        snv['assembly'] = 'GRCh38'
+
+    snv['chr'] = i['Chrom']
+
+    snv['start'] = i['Pos']
+
+    snv['ref_allele'] = i['RefAllele']
+    snv['alt_allele'] = i['AltAllele']
+
+    if re.match("homozygous$", str(i['Phenotype']), flags=re.I): #re.I == ignorecase
+        snv['genotype'] = 'Homozygous'
+    elif re.match("homozygous$", str(i['Comment']), flags=re.I):
+        snv['genotype'] = 'Homozygous'
+    else:
+        snv['genotype'] = 'Heterozygous'
+
+    snv['user_transcript'] = i['Transcript']
+
+    classification = i['Classification']
+    if classification == '3':
+        snv['pathogenicity'] = 'Uncertain'
+    elif classification == '4':
+        snv['pathogenicity'] = 'Likely pathogenic'
+    elif classification == '5':
+        snv['pathogenicity'] = 'Pathogenic'
+    
+    # Creating JSON
+    snvdata = json.dumps([snv])
+    # Posting SNV
+    response = POST(snv_url, keys, snvdata)
+    # Extracting the SNV_id and adding them to a list. 
+    JSONsnv = response.json()
+    id_snv = (JSONsnv[0]['patient_snv_id'])
+    return snv, id_snv
+
+# Function to post CNV after collecting information
+def post_CNV(patients_id, i):
+    cnv_url = URL('/patients/{0}/cnvs'.format(patients_id))
+    cnv = {}
+    cnv['patient_id'] = patients_id
+    cnv['chr'] = i['Chrom']
+    
+    if i['Assembly'] == 'GRCh37':
+        cnv['assembly'] = 'GRCh37/hg19'
+    elif i['Assembly'] == 'GRCh38':
+        cnv['assembly'] == 'GRCh38'
+
+    cnv['start'] = i['Start']
+    cnv['end'] = i['End']
+    cnv['variant_class'] = i['VarType']
+
+    if re.match('homozygous$', str(i['Phenotype']), flags=re.I):
+        cnv['genotype'] = 'Homozygous'
+    elif re.match('homozygous$', str(i['Comment']), flags=re.I):
+        cnv['genotype'] = 'Homozygous'
+    else:
+        cnv['genotype'] = 'Heterozygous' 
+    
+    classification = i['Classification']
+    if classification == '3':
+        cnv['pathogenicity'] = 'Uncertain'
+    elif classification == '4':
+        cnv['pathogenicity'] = 'Likely pathogenic'
+    elif classification == '5':
+        cnv['pathogenicity'] = 'Pathogenic'
+
+    # Creating JSON
+    cnvdata = json.dumps([cnv])
+    # Posting SNV
+    response = POST(cnv_url, keys, cnvdata)
+    # Extracting the SNV_id and adding them to a list.
+    JSONcnv = response.json()
+    id_cnv = (JSONcnv[0]['patient_cnv_id'])
+    return cnv, id_cnv
+
+# Checking log file for patient. 
+def check_previous_patients(df, old_df_row):
+    for index, row in df.iterrows():
+        if (str(row["PatientID"]) == str(old_df_row['PatientID'])) & (str(row["FamilyID"]) == str(old_df_row['FamilyID'])):
+            patients_id = row['DECIPHER_PatientID']
+            break
+        else:
+            patients_id = None
+    
+    return patients_id
+
+# creating a header variable with the authentication and content_type information - does not need to be repeated as it is uniform
+
+config = yaml.safe_load(open("DECIPHER_config.yml"))
+ 
+system_key = (config['X-Auth-Token-System'])
+user_key = (config['X-Auth-Token-User'])
+
+
+keys = {
+    'X-Auth-Token-System':system_key,
+    'X-Auth-Token-User':user_key,
+    'Content-Type': 'application/json',
+}
+
 
 # Getting project code from info endpoint 
 url = URL('/info')
@@ -56,135 +172,65 @@ response = GET(url, keys)
 
 project = response.json()
 project_id = (project['user']['project']['project_id'])
+#project_id = 'test'
 
-# Formatting of the reference value
-
-with open('reference.txt','r+') as f:
-    lines = f.readlines()
-    number = int(lines[0])
-    reference = lines[1].strip() + lines[0].strip()
-    f.seek(0)
-    f.write(str(number + 1))
-
-# Creation of a patient dictionary containing all the required fields 
-patient = {}
-patient['project_id'] = project_id
-patient['sex'] = 'unknown'
-patient['reference'] = reference 
-
-# Creating the patient on DECIPHER - first URL is assigned.
-url = URL('/projects/{0}/patients'.format(project_id))
-
-# Dictionary is converted to JSON - need to put [] around the dictionary so the resulting json is in the right format.
-patientdata = json.dumps([patient])
-
-# Patient is created within DECIPHER & the patients_id is found 
-response = POST(url, keys, patientdata)
-
-patient = response.json()
-patients_id = (patient[0]['patient_id'])
-
+# Open each csv file into a pandas dataframe
 for filepath in glob.iglob('to_upload\*.csv'):
-    # Turn each row of the csv file into a dictionary
-    with open(filepath) as csvfile:
-        reader = csv.DictReader(csvfile, delimiter = ",")
-        dicts = list(reader) 
+    var_df = pd.read_csv(filepath)
+    var_df.dropna(how='all')
     
-    # Assigning the url for creating snvs and cnvs.
-    snv_url = URL('/patients/{0}/snvs'.format(patients_id))
-    cnv_url = URL('/patients/{0}/cnvs'.format(patients_id))
+    for index, row in var_df.iterrows():
+       my_file = Path("to_upload/patient_log.csv")
+       
+       # If patient log file exists load into dataframe 
+       if my_file.exists():
+           df = pd.read_csv('to_upload/patient_log.csv')
+           # Checking patient log CSV if patients already occur. 
+           patients_id = check_previous_patients(df, row)
+           
+           # If patient is present only upload the variants 
+           if not (patients_id is None):
+               if row['Variant'] == 'SNV':
+                   post_SNV(patients_id, row)
     
-    snv_ids = [] #list to hold the ids for the created snvs
-    cnv_ids = []
-    filtered_snv = []
-    filtered_cnv = [] 
+               elif row['Variant'] == 'CNV':
+                   post_CNV(patients_id, row)
+           
+            # If patient not in log, create new patient, upload variant and then update log. 
+           else:
+               old = df.loc[df.index[-1], "Reference"]
+               oxf, num = str(old).split('_')
+               num = int(num) + 1
+               reference = oxf + '_' + str(num)
+               patients_id = create_patient(project_id, reference)
+               #patients_id = 2
+           
+               if row['Variant'] == 'SNV':
+                   post_SNV(patients_id, row)
     
-    for f in dicts:
-        if f['Variant'] == 'CNV':
-            filtered_cnv.append(f)
-        elif f['Variant'] == 'SNV':
-            filtered_snv.append(f)
-    
-    # Pull out the relevant information from the dictionaries into JSON for upload. 
-    for i in filtered_snv: 
-        snv = {}
-        snv['patient_id'] = patients_id
-    
-        if i['Assembly'] == 'GRCh37':
-            snv['assembly'] = 'GRCh37/hg19'
-        elif i['Assembly'] == 'GRCh38':
-            snv['assembly'] = 'GRCh38'
-        else:
-            print('Check transcript')
-    
-        snv['chr'] = i['Chrom']
-    
-        snv['start'] = i['Pos']
-    
-        snv['ref_allele'] = i['RefAllele']
-        snv['alt_allele'] = i['AltAllele']
-    
-        if re.match("homozygous$", i['Phenotype'], flags=re.I): #re.I == ignorecase
-            snv['genotype'] = 'Homozygous'
-        elif re.match("homozygous$", i['Comment'], flags=re.I):
-            snv['genotype'] = 'Homozygous'
-        else:
-            snv['genotype'] = 'Heterozygous'
-    
-        snv['user_transcript'] = i['Transcript']
-    
-        classification = i['Classification']
-        if classification == '3':
-            snv['pathogenicity'] = 'Uncertain'
-        elif classification == '4':
-            snv['pathogenicity'] = 'Likely pathogenic'
-        elif classification == '5':
-            snv['pathogenicity'] = 'Pathogenic'
-        
-        # Creating JSON
-        snvdata = json.dumps([snv])
-        # Posting SNV
-        response = POST(snv_url, keys, snvdata)
-        # Extracting the SNV_id and adding them to a list. 
-        JSONsnv = response.json()
-        id_snv = (JSONsnv[0]['patient_snv_id'])
-        snv_ids.append(id_snv)
-    
-    
-    for i in filtered_cnv:
-        cnv = {}
-        cnv['patient_id'] = patients_id
-        cnv['chr'] = i['Chrom']
-        
-        if i['Assembly'] == 'GRCh37':
-            cnv['assembly'] = 'GRCh37/hg19'
-        elif i['Assembly'] == 'GRCh38':
-            cnv['assembly'] == 'GRCh38'
-    
-        cnv['start'] = i['Start']
-        cnv['end'] = i['End']
-        cnv['variant_class'] = i['VarType']
-    
-        if re.match('homozygous$', i['Phenotype'], flags=re.I):
-            cnv['genotype'] = 'Homozygous'
-        elif re.match('homozygous$', i['Comment'], flags=re.I):
-            cnv['genotype'] = 'Homozygous'
-        else:
-            cnv['genotype'] = 'Heterozygous' 
-        
-        classification = i['Classification']
-        if classification == '3':
-            cnv['pathogenicity'] = 'Uncertain'
-        elif classification == '4':
-            cnv['pathogenicity'] = 'Likely pathogenic'
-        elif classification == '5':
-            cnv['pathogenicity'] = 'Pathogenic'
-    
-        # Creating JSON
-        cnvdata = json.dumps([cnv])
-        # Posting SNV
-        response = POST(cnv_url, keys, cnvdata)
-        # Extracting the SNV_id and adding them to a list.
-        JSONcnv = response.json()
-        id_cnv = (JSONcnv[0]['patient_cnv_id'])
-        cnv_ids.append(id_cnv)
+               elif row['Variant'] == 'CNV':
+                   post_CNV(patients_id, row)
+               
+               PatientID = row['PatientID']
+               FamilyID = row['FamilyID']
+               patient_data = {'Reference':reference, 'PatientID':PatientID, 'FamilyID':FamilyID, "DECIPHER_PatientID":patients_id}
+               df = df.append(patient_data, ignore_index=True)
+               df.to_csv('to_upload/patient_log.csv', index=False)
+       
+       # If no log file, create patient, upload variant and create log file. 
+       else:
+           reference = 'OXF_1'
+           patients_id = create_patient(project_id, reference)
+           patients_id = 1
+           
+           if row['Variant'] == 'SNV':
+               post_SNV(patients_id, row)
+
+           elif row['Variant'] == 'CNV':
+               post_CNV(patients_id, row)
+           
+           PatientID = row['PatientID']
+           FamilyID = row['FamilyID']
+           patient_data = {"Reference":reference, "PatientID":PatientID, "FamilyID":FamilyID, "DECIPHER_PatientID":patients_id}
+           df = pd.DataFrame(data = patient_data,  index=[0])
+           df.to_csv('to_upload/patient_log.csv', index=False)
